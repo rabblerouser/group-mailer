10e93bc836191214784ad068224c86720be6331d
Cam Jackson <camjackson89@gmail.com>
2017-09-02 11:34:25 +0800

    [Robin/Cam] Handle member-removed events

--- src/reducers/members.js
+++ src/reducers/members.js
@@ -4,6 +4,8 @@ const members = (state = defaultState, action) => {
   switch (action.type) {
     case 'CREATE_MEMBER':
       return state.concat(action.member);
+    case 'DELETE_MEMBER':
+      return state.filter(member => member.id !== action.member.id);
     default:
       return state;
   }
--- test/reducers/membersTest.js
+++ test/reducers/membersTest.js
@@ -5,6 +5,10 @@ describe('members reducer', () => {
     expect(members(undefined, {})).to.eql([]);
   });
 
+  it('preserves existing state for uninteresting actions', () => {
+    expect(members([{ id: 'blah' }], { type: 'NOT_INTERESTING' })).to.eql([{ id: 'blah' }]);
+  });
+
   it('creates a member', () => {
     const state = [{ id: 'existing-member' }];
     expect(members(state, { type: 'CREATE_MEMBER', member: { id: 'blah' } })).to.eql([
@@ -13,7 +17,9 @@ describe('members reducer', () => {
     ]);
   });
 
-  it('preserves existing state for uninteresting actions', () => {
-    expect(members([{ id: 'blah' }], { type: 'NOT_INTERESTING' })).to.eql([{ id: 'blah' }]);
+  it('deletes a member', () => {
+    const event = { type: 'DELETE_MEMBER', member: { id: 'existing-member' } };
+    const state = [{ id: 'existing-member' }, { id: 'preserved-member' }];
+    expect(members(state, event)).to.eql([{ id: 'preserved-member' }]);
   });
 });
